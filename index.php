860e4a0541f3a16c36327528e9ed96e81db98364
Adeyemi Adeshina <adeyemiadeshina6@gmail.com>
2026-03-29 21:14:30 +0100

    exindex

--- index.php
+++ index.php
@@ -892,3 +892,80 @@ func main() {
 	fmt.Print(WordFlip("     "))
 	fmt.Print(WordFlip(" hello  all  of  you! "))
 }
+
+
+func PrintMemory(arr [10]byte) {
+	hexChars := "0123456789abcdef"
+
+	// --- HEX DISPLAY (4 per line) ---
+	for i := 0; i < len(arr); i++ {
+
+		firstDigit := arr[i] / 16
+		secondDigit := arr[i] % 16
+
+		z01.PrintRune(rune(hexChars[firstDigit]))
+		z01.PrintRune(rune(hexChars[secondDigit]))
+
+		// Space if not end of line or last element
+		if (i+1)%4 != 0 && i != len(arr)-1 {
+			z01.PrintRune(' ')
+		}
+        
+		// New line every 4 bytes
+		if (i+1)%4 == 0 {
+			z01.PrintRune('\n')
+		}
+	}
+
+	// If last line not complete
+	if len(arr)%4 != 0 {
+		z01.PrintRune('\n')
+	}
+
+	// --- ASCII DISPLAY ---
+	for i := 0; i < len(arr); i++ {
+		if arr[i] >= 32 && arr[i] <= 126 {
+			z01.PrintRune(rune(arr[i]))
+		} else {
+			z01.PrintRune('.')
+		}
+	}
+	z01.PrintRune('\n')
+}
+
+// union
+func alreadySeen(str string, c rune, index int) bool {
+	for i := 0; i < index; i++ {
+		if rune(str[i]) == c {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		z01.PrintRune('\n')
+		return
+	}
+
+	s1 := os.Args[1]
+	s2 := os.Args[2]
+
+	// check first string
+	for i, c := range s1 {
+		if !alreadySeen(s1, c, i) {
+			z01.PrintRune(c)
+		}
+	}
+
+	// check second string
+	for i, c := range s2 {
+		if !alreadySeen(s1+s2, c, len(s1)+i) {
+			z01.PrintRune(c)
+		}
+	}
+
+	z01.PrintRune('\n')
+}
+// end union
